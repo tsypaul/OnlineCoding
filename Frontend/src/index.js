daab387d0528d5494e0375bc614c8a7cbd2bc952
Shaoyu Tan <tsypaul@outlook.com>
2018-11-20 15:21:26 -0500

    put npm start script to server package.json, and finished configuring front end with webpack and babel

---
+++ Frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'online coding project';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
