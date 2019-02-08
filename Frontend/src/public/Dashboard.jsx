8c3040752d548d53d028b5e096a7c823abb70e93
Shaoyu Tan <tsypaul@outlook.com>
2019-02-08 12:53:39 -0500

    commit what I have

--- Frontend/src/public/Dashboard.jsx
+++ Frontend/src/public/Dashboard.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import Axios from 'axios';
 
 export default class Dashboard extends Component {
+
+  state={
+    visible: true,
+    filename: ''
+  }
+
+  newProject(){
+    
+  }
+
+  submitNewProject(){
+    Axios({
+      method: 'POST',
+      url: '/code',
+      name: ''
+    }).then(
+      (res)=>{
+        if(res.data == 'exist'){
+
+        }
+      }
+    )
+  }
+
   render() {
     return (
       <div>
-        
+        <button onClick={this.newProject}>New Project</button>
+        {this.state.visible?
+        <input value={this.state.filename} placeholder="filename with extension"/>:null}
       </div>
     )
   }
