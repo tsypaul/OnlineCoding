fa744bf4faad6024781eec1daea0b87692e14d72
Ali Faizan <alifaizann95@gmail.com>
2019-02-25 15:25:01 -0500

    Login

---
+++ webapp/client/src/components/layout/Landing.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Welcome</b> to the 4th Year Project Portal
+
+            </h4>
+            <br />
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable red accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable green accent-3 white-text"
+              >
+                Log In
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
---
+++ webapp/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://ali:m12345@ds343895.mlab.com:43895/webapp",
+  secretOrKey: "secret"
+};
